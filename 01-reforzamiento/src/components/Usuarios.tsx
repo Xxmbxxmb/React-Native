9a8bfe2505ec010527f449e4fcb7f817f56d1fb9
Diego Cerda <diegocerdacelis@hotmail.com>
2022-10-12 10:28:41 -0300

    New map users

---
+++ 01-reforzamiento/src/components/Usuarios.tsx
@@ -0,0 +1,51 @@
+import { useUsuario } from "../hooks/useUsuarios";
+import { Usuario } from '../interfaces/reqRes';
+
+export const Usuarios = () => {
+
+  const { usuarios, paginaAnterior, paginaSiguiente } = useUsuario()
+
+  const renderUser = ({id, first_name, last_name, email, avatar}: Usuario) => {
+    return (
+      <tr key={id}>
+        <td>
+          <img
+            src={`${avatar}`}
+            alt=""
+            style={{ width: 50, borderRadius: 100 }}
+          />
+        </td>
+        <td>
+          {first_name} {last_name}
+        </td>
+        <td>{email}</td>
+      </tr>
+    );
+  };
+
+  return (
+    <>
+      <h3>Usuarios: </h3>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Avatar</th>
+            <th>Nombre</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>{usuarios.map(renderUser)}</tbody>
+      </table>
+
+      <button className="btn btn-primary" onClick={paginaAnterior}>
+        Anteriores
+      </button>
+
+      &nbsp;
+
+      <button className="btn btn-primary" onClick={paginaSiguiente}>
+        Siguientes
+      </button>
+    </>
+  );
+};
