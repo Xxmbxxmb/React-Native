4ddeeb79035309c0014c136c87f4391b727932e5
Diego Cerda <diegocerdacelis@hotmail.com>
2022-10-13 22:55:24 -0300

    Upload first proyect react-native

---
+++ PrimerProyecto/src/components/Fab.tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import {
+  StyleSheet,
+  TouchableNativeFeedback,
+  View,
+  Text,
+  TouchableOpacity,
+  Platform,
+} from 'react-native';
+
+interface Props {
+  title: string;
+  position?: 'br' | 'bl';
+  onPress: () => void;
+}
+
+export const Fab = ({title, position = 'br', onPress}: Props) => {
+  const ios = () => {
+    return (
+      <TouchableOpacity
+      onPress={onPress}
+        style={[
+          styles.fabLocation,
+          position === 'br' ? styles.right : styles.left,
+        ]}>
+        <View style={styles.fab}>
+          <Text style={styles.fabText}>{title}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
+  const android = () => {
+    return (
+      <View
+        style={[
+          styles.fabLocation,
+          position === 'br' ? styles.right : styles.left,
+        ]}>
+        <TouchableNativeFeedback
+          onPress={onPress}
+          background={TouchableNativeFeedback.Ripple('black', false, 30)}>
+          <View style={styles.fab}>
+            <Text style={styles.fabText}>{title}</Text>
+          </View>
+        </TouchableNativeFeedback>
+      </View>
+    );
+  };
+
+  return Platform.OS === 'android' ? android() : ios();
+};
+
+const styles = StyleSheet.create({
+  fab: {
+    backgroundColor: '#5856D6',
+    width: 60,
+    height: 60,
+    borderRadius: 100,
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.27,
+    shadowRadius: 4.65,
+    elevation: 6,
+  },
+  fabLocation: {
+    position: 'absolute',
+    bottom: 25,
+  },
+  right: {
+    right: 25,
+  },
+  left: {
+    left: 25,
+  },
+  fabText: {
+    color: 'white',
+    fontSize: 25,
+    fontWeight: 'bold',
+    alignSelf: 'center',
+  },
+});
