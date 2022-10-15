54b46bca7576c0e6214325a5ebc9f51ee5447571
Diego Cerda <diegocerdacelis@hotmail.com>
2022-10-14 23:30:49 -0300

    tarea upload

---
+++ 02-PrimeraApp/src/screens/TareaScreen.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import {View, StyleSheet, useWindowDimensions} from 'react-native';
+
+export const TareaScreen = () => {
+    const {height, width} = useWindowDimensions();
+  return (
+    <View style={styles.container}>
+      <View style={styles.cajaMorada} />
+      <View style={styles.cajaNaranja} />
+      <View style={styles.cajaAzul} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#28425B',
+    justifyContent: 'space-between',
+  },
+  cajaMorada: {
+    width: 100,
+    height: 100,
+    borderWidth: 10,
+    borderColor: 'white',
+    backgroundColor: '#5856D6',
+    alignSelf: 'flex-end',
+  },
+  cajaNaranja: {
+    width: 100,
+    height: 100,
+    borderWidth: 10,
+    borderColor: 'white',
+    backgroundColor: '#F0A23B',
+    alignSelf: 'center',
+  },
+  cajaAzul: {
+    width: 100,
+    height: 100,
+    borderWidth: 10,
+    borderColor: 'white',
+    backgroundColor: '#28C4D9',
+  },
+});
