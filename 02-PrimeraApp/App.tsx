8824c7d4b6bded5522ffbdc22fb151fd1ce25ed2
Diego Cerda <diegocerdacelis@hotmail.com>
2022-10-14 23:33:54 -0300

    test

--- 02-PrimeraApp/App.tsx
+++ 02-PrimeraApp/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
-import {ContadorScreen} from './src/screens/ContadorScreen';
+import {SafeAreaView} from 'react-native';
+import { TareaScreen } from './src/screens/TareaScreen';
+// import {ContadorScreen} from './src/screens/ContadorScreen';
+// import {DimensionScreen} from './src/screens/DimensionScreen';
+// import {PositionScreen} from './src/screens/PositionScreen';
 // import {HolaMundoScreen} from './src/screens/HolaMundoScreen';
 
 const App = () => {
   return (
-    // <HolaMundoScreen />
-    <ContadorScreen />
+    <SafeAreaView style={{
+      flex: 1,
+      backgroundColor: '#28425B',
+    }}>
+      {/* <HolaMundoScreen />
+      <ContadorScreen />
+      <DimensionScreen /> */}
+      {/* <PositionScreen /> */}
+      <TareaScreen />
+    </SafeAreaView>
   );
 };
 
