bff7965983f8186a4efdb12b3b44d2d50f173afc
Diego Cerda <diegocerdacelis@hotmail.com>
2022-10-18 10:33:58 -0300

    new dependencies

--- 04-Navigation/.eslintrc.js
+++ 04-Navigation/.eslintrc.js
@@ -11,6 +11,8 @@ module.exports = {
         'no-shadow': 'off',
         'no-undef': 'off',
         'react-native/no-inline-styles': 0,
+        'react-hooks/exhaustive-deps': 0,
+        '@typescript-eslint/no-unused-vars': 0,
         curly: 0,
         'prettier/prettier': [
           'error',
