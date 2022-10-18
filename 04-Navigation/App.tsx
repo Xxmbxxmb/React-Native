87e2f3e2330063392cf6ec3be7f77649660624e5
Diego Cerda <diegocerdacelis@hotmail.com>
2022-10-17 22:52:07 -0300

    Add navigator

--- 04-Navigation/App.tsx
+++ 04-Navigation/App.tsx
@@ -1,7 +1,8 @@
 import 'react-native-gesture-handler';
+
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { StackNavigator } from './src/navigator/StackNavigator';
+import { StackNavigator } from './src/navigator/Navigation';
 
 const App = () => {
   return (
