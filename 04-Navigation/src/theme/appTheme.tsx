f6d33fb73fe26b828d2d1039e246291430da10d9
Diego Cerda <diegocerdacelis@hotmail.com>
2022-10-18 10:35:23 -0300

    New themes for navigators

--- 04-Navigation/src/theme/appTheme.tsx
+++ 04-Navigation/src/theme/appTheme.tsx
@@ -1,6 +1,42 @@
 import { StyleSheet } from 'react-native';
+
 export const styles = StyleSheet.create({
   globalMargin: {
     marginHorizontal: 20,
+  },
+  title: {
+    fontSize: 30,
+    marginBottom: 10,
+  },
+  botonGrande: {
+    width: 100,
+    height: 100,
+    borderRadius: 20,
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginHorizontal: 10,
+  },
+  botonGrandeTexto: {
+    color: 'white',
+  },
+  avatarContainer: {
+    alignItems: 'center',
+    marginTop: 15,
+  },
+  avatar: {
+    width: 100,
+    height: 100,
+    borderRadius: 100,
+  },
+  menuContainer: {
+    marginVertical: 30,
+    marginHorizontal: 30,
+  },
+  menuBtn: {
+    paddingVertical: 10,
+  },
+  textoMenu: {
+    fontSize: 20,
   },
 });
