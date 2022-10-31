e96c94b204c068072c3d3575523ee1a21d4f4451
Diego Cerda <diegocerdacelis@hotmail.com>
2022-10-31 14:06:57 -0300

    new component

---
+++ 05-RNComponents/src/components/ItemSeparator.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export const ItemSeparator = () => {
+  return (
+    <View style={{ borderBottomWidth: 2, marginVertical: 8, opacity: 0.4 }} />
+  );
+};
